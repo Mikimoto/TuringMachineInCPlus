ea656e61ad9d38220458e2e50b3b558955c63483
Mikimoto <mikimoto.c@gmail.com>
2022-10-30 19:09:20 +0800

    Initial Commit

---
+++ TuringMachine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TuringMachine
+//
+//  Created by Mikimoto on 2022/10/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
